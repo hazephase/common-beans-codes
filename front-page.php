bb26584f67624b38ba19bf6afc20e913b1406dab
Elroy Fernandes <hazephase@users.noreply.github.com>
2018-02-26 16:58:29 +0530

    Update front-page.php

--- front-page.php
+++ front-page.php
@@ -6,5 +6,8 @@
 /**
  * Enqueue UIkit assets.
  */
+
+// removes out all post content from the backend
+beans_remove_action( 'beans_loop_template' );
 remove_all_actions( 'beans_post_body' );
 beans_load_document();

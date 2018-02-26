1cc2dae1cc9dc623195b8d2fc52c82579b9c7328
Elroy Fernandes <hazephase@users.noreply.github.com>
2018-02-26 16:25:05 +0530

    Update front-page.php

--- front-page.php
+++ front-page.php
@@ -1,1 +1,10 @@
+<?php
+/* Template Name: home page */
 
+// Do all your page template modification here.
+//add_action( 'beans_uikit_enqueue_scripts', 'example_view_enqueue_uikit_assets' );
+/**
+ * Enqueue UIkit assets.
+ */
+
+beans_load_document();
